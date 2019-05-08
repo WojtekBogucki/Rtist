2b9427536dd2963776449bd91194bdd143a720ce
Wojciech Bogucki <boguckiw@student.mini.pw.edu.pl>
2019-05-08 18:02:37 +0200

    Dodanie winietki

--- R/Rzeka.R
+++ R/Rzeka.R
@@ -2,7 +2,9 @@
 #'
 #' @description Funkcja rysująca obraz pt. "Rzeka"
 #'
-#' @param bg_color - kolor tła
+#' @param bg_color - kolor tła podany jako tekst
+#'
+#' @usage Rzeka("red")
 #'
 #' @export
 Rzeka <- function(bg_color="gold"){
@@ -14,11 +16,11 @@ Rzeka <- function(bg_color="gold"){
 
   par(bg=bg_color)
   par(mar=c(0,0,0,0))
-  # rgb2hsv(col2rgb("red"))[1]
+  bg_hue <- rgb2hsv(col2rgb(bg_color))[1]
 
   plot(seq(-2,2,0.01),pnorm(seq(-2,2,0.01)), type="s", col=3, axes=FALSE, ann = FALSE)
   for(i in rep(100:500, 3)){
-    lines(sort(rnorm(i, mean=ifelse(i%%2,-1,1)*(i-300)/2000)),(1:i)/i,type="s", col=rainbow(800)[i])
+    lines(sort(rnorm(i, mean=ifelse(i%%2,-1,1)*(i-300)/2000)),(1:i)/i,type="s", col=rainbow(600, start = bg_hue)[i-100])
   }
   par(bg=old_bg)
   par(mar=old_mar)
