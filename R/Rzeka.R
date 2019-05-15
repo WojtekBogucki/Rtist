35cbd7f134a5ca84121f495f31ae86c02427f090
MatiFilozof <32573962+MatiFilozof@users.noreply.github.com>
2019-05-15 15:47:17 +0200

    upgraded Rzeka

--- R/Rzeka.R
+++ R/Rzeka.R
@@ -1,38 +1,27 @@
-#' @name Rzeka
-#'
-#' @author Wojciech Bogucki
-#' @author Karol Pysiak
-#'
-#'
-#' @title Plotting river
-#'
-#' @description This function helps us when we want to see an image of a river. It creates that by plotting
-#' many cumulative normal distributions. We can change the color of a background to any colour we want.
-#'
-#' @param bg_color - background color in form of a text
-#'
-#' @usage Rzeka(bg_color = "gold")
-#'
-#' @examples
-#' Rzeka()
-#' Rzeka(bg_color = "red")
-#' Rzeka(bg_color = "blue")
-#'
-#' @export
-Rzeka <- function(bg_color="gold"){
 
+Rzeka <- function (params = list(dist = "t", df = 99), bg_color = "gold") {
   old_bg <- par("bg")
   old_mar <- par("mar")
-
-  par(bg=bg_color)
-  par(mar=c(0,0,0,0))
+  par(bg = bg_color)
+  par(mar = c(0, 0, 0, 0))
   bg_hue <- rgb2hsv(col2rgb(bg_color))[1]
-
-  plot(seq(-2,2,0.01),pnorm(seq(-2,2,0.01)), type="s", col=3, axes=FALSE, ann = FALSE)
-  for(i in rep(100:500, 3)){
-    lines(sort(rnorm(i, mean=ifelse(i%%2,-1,1)*(i-300)/2000)),(1:i)/i,type="s", col=rainbow(600, start = bg_hue)[i-100])
+  plot(seq(-4, 4, 0.01), pnorm(seq(-4, 4, 0.01)), type = "s", 
+       col = bg_color, axes = FALSE, ann = FALSE)
+  for (i in rep(100:500, 3)) {
+    if (params$dist == "t") {
+      lines(sort(rt(i, df = params$df, ncp = ifelse(i%%2, -1, 1) * (i - 300)/2000)),
+            (1:i)/i, type = "s", col = rainbow(600, start = bg_hue)[i - 100])
+    } else if (params$dist == "norm") {
+      lines(sort(rnorm(i, mean = ifelse(i%%2, -1, 1) * (i - 300)/2000)),
+            (1:i)/i, type = "s", col = rainbow(600, start = bg_hue)[i - 100])
+    } else if (params$dist == "lnorm") {
+      lines(sort(rlnorm(i, meanlog = ifelse(i%%2, -1, 1) * (i - 300)/2000)),
+            (1:i)/i, type = "s", col = rainbow(600, start = bg_hue)[i - 100])
+    } else if (params$dist == "binom") {
+      lines(sort(rbinom(i, size = params$size, prob = params$prob) + ifelse(i%%2, -1, 1) * (i - 300)/2000),
+            (1:i)/i, type = "s", col = rainbow(600, start = bg_hue)[i - 100])
+    }
   }
-  par(bg=old_bg)
-  par(mar=old_mar)
-
+  par(bg = old_bg)
+  par(mar = old_mar)
 }
